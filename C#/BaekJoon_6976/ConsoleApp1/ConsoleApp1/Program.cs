64bbf98955e0bba2808ab6e5b688f933c8186899
LeeJaeJun <jaejoon4929@gmail.com>
2021-11-12 09:51:26 +0900

    수정

--- C#/BaekJoon_6976/ConsoleApp1/ConsoleApp1/Program.cs
+++ C#/BaekJoon_6976/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,32 +10,44 @@ namespace ConsoleApp1
 
             String input_number = Console.ReadLine();
             ulong result = Convert.ToUInt64(input_number);
+
             while (true)
             {
                 if (result <= 11)
                 {
                     if (result == 11)
                     {
-                        Console.WriteLine("The number " + input_number + " is divisible by 11.");
+                        Console.WriteLine("The number {0} is divisible by 11.", input_number);
                         break;
                     }
                     else
                     {
-                        Console.WriteLine("The number " + input_number + " is not divisible by 11.");
+                        Console.WriteLine("The number {0} is not divisible by 11.", input_number);
                         break;
                     }
                 }
                 else
                 {
                     String temp = result.ToString();
+                    String temp_lastnum = "";
                     if (temp.Length >= 0)
                     {
-                        temp = temp.Substring(0, temp.Length - 1);
-                        String temp_lastnum =
-                            temp.Substring(temp.Length - 1, int.Parse(input_removeCount));
-
-                        result = Convert.ToUInt64(temp) - Convert.ToUInt64(temp_lastnum);
-                        Console.WriteLine(result);
+                        try
+                        {
+                            temp = temp.Substring(0, temp.Length - 1);
+                            temp_lastnum =
+                                temp.Substring(temp.Length - 1, int.Parse(input_removeCount));
+                        }
+                        catch
+                        {
+                            Console.WriteLine("You are Error");
+                            break;
+                        }
+                        finally
+                        {
+                            result = Convert.ToUInt64(temp) - Convert.ToUInt64(temp_lastnum);
+                            Console.WriteLine(result);
+                        }
                     }
                     else
                     {
