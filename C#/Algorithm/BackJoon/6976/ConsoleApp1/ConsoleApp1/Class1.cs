76d121458f0aff26a77a31e243d9b43c3ec9cd84
LeeJaeJun <jaejoon4929@gmail.com>
2021-11-17 18:06:24 +0900

    Fixed dir

---
+++ C#/Algorithm/BackJoon/6976/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        delegate void MyDelegate(string Name, int age);
+        delegate string Message(string message);
+
+        static void Main(string[] args)
+        {
+            MyDelegate student = (name, age) =>
+            {
+                Console.WriteLine("이름 : {0}, 나이 : {1}", name, age);
+            };
+
+            student("범범조조", 27);
+
+            Message message = (str) => { return str; };
+            Console.WriteLine("이름 : {0}", message("범범조조"));
+
+        }
+    }
+}
---
+++ C#/Algorithm/BackJoon/6976/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+
+//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            String input_removeCount = Console.ReadLine();
+//            String input_number = Console.ReadLine();
+
+//            ulong result = Convert.ToUInt64(input_number);  //String to ulong
+//            while (true)
+//            {
+//                if (result <= 99)   //check 99 ~ 0
+//                {
+//                    if (result == 11) //if didvisable 11
+//                    {
+//                        Console.WriteLine("The number " + input_number + " is divisible by 11.");
+//                        break;
+//                    }
+//                    else //if not didvisable 11
+//                    {
+//                        Console.WriteLine("The number " + input_number + " is not divisible by 11.");
+//                        break;
+//                    }
+//                }
+//                else //check 100 ~ N
+//                {
+//                    String temp = result.ToString();    //ulong to String
+//                    int lastindex = Int32.Parse(input_removeCount);
+//                    if (result.ToString().Length <= 0)  //Check ArgumentOutOfRange
+//                    {
+//                        break;
+//                    }
+//                    else
+//                    {
+//                        String temp_Delelastnum = temp.Substring(0, temp.Length - 1);
+//                        String temp_Justlastnum = temp.Substring(temp.Length - 1, lastindex);
+
+//                        result =
+//                            Convert.ToUInt64(temp_Delelastnum) - Convert.ToUInt64(temp_Justlastnum); //Rewrite
+//                        Console.WriteLine(result);
+//                        //return to take result(ulong)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
