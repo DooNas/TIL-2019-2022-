0a0816a6cca3497f32bf0fcd106d33c9a4266118
LeeJaeJun <jaejoon4929@gmail.com>
2021-11-26 00:00:40 +0900

    OpenApi 클래스 분할

--- Mini_Project/OpenAPI/Civ19_WithCsharp/Civ19_WithCsharp/Civ19_WithCsharp/Form1.cs
+++ Mini_Project/OpenAPI/Civ19_WithCsharp/Civ19_WithCsharp/Civ19_WithCsharp/Form1.cs
@@ -24,41 +24,12 @@ namespace Civ19_WithCsharp
         {
             InitializeComponent();
         }
-        static HttpClient client = new HttpClient();
-
-        static void OpenApiGetFile()
-        {
-            //데이터 호출(Xml)
-            string url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"; // URL
-            url += "?ServiceKey=" + ApiKey.Getkey(); // Service Key
-            //url += "&pageNo=1";
-            //url += "&numOfRows=10";
-            url += "&startCreateDt=20211124";
-            //url += "&endCreateDt=20211124";
-
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-
-            String results = string.Empty;
-            HttpWebResponse response;
-            using (response = request.GetResponse() as HttpWebResponse)
-            {
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-                results = reader.ReadToEnd();
-            }
-            //Xml 파일처리
-            StreamWriter writer;
-            writer = File.CreateText("Civ19.xml");
-            writer.Write(results);
-            writer.Close();
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             richTextBox1.Clear();
-            OpenApiGetFile();
+            OpenApi_GetXml.OpenApiGetFile();
             //xml에서 어떻게 해야 현제 확진자 수를 추출할 수 있을까?
-            ////Xml을 Json형태로 받아보았다. [실패]
             ////Xml에서 파싱을 해보자.
             try
             {
@@ -66,13 +37,10 @@ namespace Civ19_WithCsharp
                 xml.Load("Civ19.xml");
 
                 XmlNodeList xnList = xml.SelectNodes("/response/body/items/item"); //접근할 노드
-
-                
                 foreach (XmlNode xn in xnList)
                 {
                     string part1 = xn["createDt"].InnerText; //작성 날짜 불러오기
                     string part2 = xn["decideCnt"].InnerText; //확진자 불러오기
-
                     richTextBox1.AppendText("작성 날짜 : "+ part1 + " \t 확진자 수 : " + part2 + "명\n\n");
                 }
                 
