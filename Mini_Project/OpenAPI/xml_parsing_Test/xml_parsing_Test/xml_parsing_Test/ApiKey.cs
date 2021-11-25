27c07f32383737096f62b11e8e365ae8f7335b99
LeeJaeJun <jaejoon4929@gmail.com>
2021-11-25 23:53:58 +0900

    Merge branch 'main' into MiniProJect/OpenApi
    # Conflicts:
    #	Mini_Project/OpenAPI/Civ19_WithCsharp/Civ19_WithCsharp/.vs/Civ19_WithCsharp/v17/.suo

---
+++ Mini_Project/OpenAPI/xml_parsing_Test/xml_parsing_Test/xml_parsing_Test/ApiKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xml_parsing_Test
+{
+    public static class ApiKey
+    {
+        public static String Getkey()
+        {
+            String key = "bLGxzflOuRiHWXK3IjCE0hyCkYR4LL9El6kRUbUKpNQvb%2F58f7O78BeP7kkrHbcAXtrv4d7TEFPIgji%2BznEE%2BQ%3D%3D";
+            return key;
+        }
+    }
+}
