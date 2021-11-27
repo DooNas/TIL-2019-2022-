f1eb323b3685dcb038a7a5749acc0aea82d055b0
LeeJaeJun <jaejoon4929@gmail.com>
2021-11-27 22:58:50 +0900

    XmlParsing 클래스 분할_ String array와 Int array 두가지로 파싱이 가능하도록 조치했다.

--- Mini_Project/OpenAPI/Civ19_WithCsharp/Civ19_WithCsharp/Civ19_WithCsharp/Form1.Designer.cs
+++ Mini_Project/OpenAPI/Civ19_WithCsharp/Civ19_WithCsharp/Civ19_WithCsharp/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -44,23 +46,39 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(437, 351);
+            this.richTextBox1.Size = new System.Drawing.Size(437, 186);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 314);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(437, 232);
+            this.dataGridView1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 375);
+            this.ClientSize = new System.Drawing.Size(622, 558);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(638, 597);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +86,7 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Mini_Project/OpenAPI/Civ19_WithCsharp/Civ19_WithCsharp/Civ19_WithCsharp/Form1.cs
+++ Mini_Project/OpenAPI/Civ19_WithCsharp/Civ19_WithCsharp/Civ19_WithCsharp/Form1.cs
@@ -24,16 +24,17 @@ namespace Civ19_WithCsharp
         public Form1()
         {
             InitializeComponent();
+            Datatable();
             button1.Click += Button1_Click;
         }
-        String Todate = DateTime.Now.ToString("yyyyMMdd");
         DataTable dt;
 
 
-        public void Datatable(String[] createDt, int[] decideCnt)
+        private void Datatable()
         {
             dt = new DataTable();
-            dt.Columns.Add();
+            dt.Columns.Add(new DataColumn("DATE", typeof(DateTime)));
+            dt.Columns.Add(new DataColumn("DECIDECNT", typeof(int)));
         }
 
         private async void Button1_Click(object sender, EventArgs e)
@@ -41,33 +42,17 @@ namespace Civ19_WithCsharp
             Thread.Sleep(100);
             richTextBox1.Clear();
 
-            OpenApi_GetXml openApi_GetXml = new OpenApi_GetXml();
-            String results = await openApi_GetXml.OpenApiGetFile(ApiKey.Getkey());
-            //await는 void, Task, Task<변수형>만 가능하다.
-            //Task<변수형>경우 자리를 예약하고 해당 자리에 예약한 손님이 앉기 전까지는 다음으로 진행하지 않는다는 의미를 갖는다.
+            OpenApi openApi = new OpenApi();
+            String results = await openApi.OpenApiGetData(ApiKey.Getkey());
+            openApi.DataToXml(results); //Make Civ19.xml
 
-
-            StreamWriter writer;
-            writer = File.CreateText("Civ19.xml");
-            writer.Write(results);
-            writer.Close();
-            //xml에서 어떻게 해야 현제 확진자 수를 추출할 수 있을까?
-            ////Xml에서 파싱을 해보자.
             try
             {
-                XmlDocument xml = new XmlDocument();
-                xml.Load("Civ19.xml");
-
-                XmlNodeList xnList = xml.SelectNodes("/response/body/items/item"); //접근할 노드
                 String[] createDt = new string[9];
+                openApi.XmlParsing_String(createDt, "createDt");
+
                 int[] decideCnt = new int[9];
-                int i = 0;
-                foreach (XmlNode xn in xnList)
-                {
-                    createDt[i] = xn["createDt"].InnerText; //작성 날짜 불러오기
-                    decideCnt[i] = int.Parse(xn["decideCnt"].InnerText); //확진자 불러오기
-                    i++;
-                }
+                openApi.XmlParsing_Int(decideCnt, "decideCnt");
                 //createDt = {'오늘날짜', '오늘날짜 - 1', '오늘날짜 - 2', '오늘날짜 - 3', '오늘날짜 - 4', '오늘날짜 - 7', '오늘날짜 - 8'}
                 //decideCnt = {'오늘확진자', '오늘확진자 - 1', '오늘확진자 - 2', '오늘확진자 - 3', '오늘확진자 - 4', '오늘확진자 - 7', '오늘확진자 - 8'}
                 for (int index = 0; index < 6; index++)
@@ -78,17 +63,7 @@ namespace Civ19_WithCsharp
                     {
                         return;
                     }
-                    richTextBox1.AppendText("날짜 : " + Today.Substring(0,10) + "\t확진자 수  : " + TodaydecideCnt + "명\n\n");
-                }
-
-                foreach (String n in createDt)
-                {
-                    richTextBox1.AppendText(n + "\n");
-                }
-                richTextBox1.AppendText("\n");
-                foreach (int n in decideCnt)
-                {
-                    richTextBox1.AppendText(n + "\n");
+                    richTextBox1.AppendText("날짜 : " + Today + "\t확진자 수  : " + TodaydecideCnt + "명\n\n");
                 }
 
             }
