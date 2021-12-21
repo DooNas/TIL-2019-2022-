a5f5922d759eddfce1054a24057dde965bd37de0
LeeJaeJun <jaejoon4929@gmail.com>
2021-12-21 20:27:55 +0900

    1110번 문제 자꾸 틀려서 내일 java로 시도해볼것

---
+++ Algorithm/BackJoon/1110/1110/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int Tennum;
+            int Onenum;
+            int settemp;
+            int resultnum;
+
+            int count = 1;
+            Console.Write("두자리 정수 입력 : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (0 <= num && num < 100)
+            {
+                resultnum = num;
+                while (true)
+                {
+                    Tennum = num / 10;
+                    Onenum = num % 10;
+                    settemp = Tennum + Onenum;
+                    num = ((num % 10) * 10) + (settemp % 10);
+                    if (resultnum == num)
+                    {
+                        break;
+                    }
+                    count++;
+                }
+            }
+            Console.Write(count);
+        }
+    }
+}
