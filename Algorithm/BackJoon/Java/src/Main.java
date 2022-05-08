473ca9ee495abf3288397ac0d08f00d3c13bb351
DooNas <jaejoon4929@gmail.com>
2022-05-08 21:12:29 +0900

    [백준 ]문제풆이

---
+++ Algorithm/BackJoon/Java/src/Main.java
@@ -0,0 +1,8 @@
+import backjoon.no10926;
+
+public class Main {
+    public static void main(String[] args) {
+        no10926 no10926 = new no10926();
+        no10926.ansower();
+    }
+}
---
+++ Algorithm/BackJoon/Java/src/backjoon/no10926.java
@@ -0,0 +1,12 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class no10926 {
+
+    public void ansower(){
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str+"??!");
+    }
+}
