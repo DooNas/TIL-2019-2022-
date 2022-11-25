4782475e7fab827c70d24a5d3eb0320cbc5425f0
DooNas <jaejoon4929@gmail.com>
2022-11-25 14:48:32 +0900

    파일 정리

--- C#/Vscode/helloworld/Program.cs
+++ C#/Vscode/helloworld/Program.cs
@@ -1,22 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace baekjoon
+class Program
 {
-    class no5597
+    public static void Main()
     {
-        static void Main(String[] args)
-        {
-            int[] list = new int[30];
-            for(int i = 0; i < 28; i++)
-            {
-                int num = Convert.ToInt32(Console.ReadLine());
-                list[num-1] = 1;
-            }
-            
-            for(int i = 0; i < 30; i++)
-            {
-                if(list[i] != 1) Console.WriteLine(i+1);
-            }
-        }
+        int[] people = {50, 70, 50, 80};
+        Console.WriteLine(solution(people, 100));
     }
+
+
+    public static int solution(int[] people, int Limit)
+    {
+        int Count = 0;
+
+        Array.Sort(people);
+        int light = 0;
+        int heavy = people.Length - 1;
+
+        while(light <= heavy)
+        {
+            if(people[light] + people[heavy] <= Limit)
+            {
+                light++;
+            }
+            heavy--;
+            Count++;
+        }
+        return Count;
+    }
+
+
 }
